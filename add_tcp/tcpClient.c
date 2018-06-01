5637dab2527c3cd71a694d3f579ebe5c996db937
weicheng <1728672994@qq.com>
2018-06-01 08:13:48 -0700

    网络版加法器

---
+++ add_tcp/tcpClient.c
@@ -0,0 +1,77 @@
+/*************************************************************************
+	> File Name: tcpClient.c
+	> Author: weicheng
+	> Created Time: Thu 26 Apr 2018 05:46:21 PM PDT
+ ************************************************************************/
+#include<signal.h>
+#include<stdio.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<errno.h>
+#include<string.h>
+#include<unistd.h>
+#include<netinet/in.h>
+#include<stdlib.h>
+#include<arpa/inet.h>
+
+typedef struct Request
+{
+    int a;
+    int b;
+}Request;
+typedef struct Response
+{
+    int sum;
+}Response;
+
+int main(int argc, char* argv[])
+{
+    if(argc != 2)
+    {
+        printf("Usage %s[ip]\n", argv[0]);
+        exit(1);
+    }
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in local;
+    local.sin_family = AF_INET;
+    inet_pton(AF_INET, argv[1], &local.sin_addr);
+    // local.sin_addr.s_addr = inet_addr(argv[1]);
+    local.sin_port = htons(8080);
+
+    int ret = connect(sock, (struct sockaddr*)&local, sizeof(local));
+    if(ret < 0)
+    {
+        printf("connect errno, error code is:%d, errstring is:%s\n", errno, strerror(errno));
+        exit(2);
+    }
+    printf("connect success!   \n");
+    while(1)
+    {
+        Request request;
+        Response response;
+        printf("please input two number\n");
+        scanf("%d, %d", &request.a, &request.b); 
+        write(sock, &request, sizeof(request));
+
+        read(sock, &response, sizeof(response));
+        printf("%d+%d = %d\n", request.a, request.b, response.sum);
+     //   char buf[1024]={0};
+     //   printf("client# ");
+     //   fflush(stdout);
+     //   ssize_t s = read(0, buf, sizeof(buf));
+     //   if(s > 0)
+     //   {
+     //       buf[s-1] = 0;
+     //       write(sock, buf, sizeof(buf));
+     //       if(strcmp(buf, "quit") == 0)
+     //       {
+     //           printf("client quit\n");
+     //           break;
+     //       }
+     //       read(sock, buf, sizeof(buf));
+     //       printf("server# %s\n", buf);
+     //   }
+    }
+    close(sock);
+    return 0;
+}
